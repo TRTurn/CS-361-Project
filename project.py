68e7b505378c97256a5f144176c9d1f142110fa5
Tim Turnbaugh <turnbaugh.tim@gmail.com>
2021-02-16 18:45:57 -0800

    Search Wikipedia now produces results and generate output changes the output_box display

--- project.py
+++ project.py
@@ -5,6 +5,7 @@ import sys
 
 if sys.argv[0]:
     pass
+
 root = tk.Tk()
 
 root.title("Content Generator")
@@ -22,44 +23,32 @@ output_box.insert(1.0, "Output will appear here")  # The 1.0 refers to line 1, c
 output_box.grid(row=3, column=0, padx=20, pady=20)
 
 
-# def generate_output():
-#     keywords = primary_key.get(1.0, tk.END)[:-1].replace(" ", "").split(",")  # The -1 removes the newline token automatically included by tkinter
-#     secondary_keyword = secondary_key.get(1.0, tk.END)
-#     if len(keywords) > 2:
-#         output_box.delete(1.0, tk.END)
-#         output_box.insert(1.0, "Insert only two keywords")
-#     elif keywords is None:
-#         pass
-#     elif len(keywords) == 1:
-#         search_wikipedia(keywords[0])
-#     else:
-#         search_wikipedia(keywords[0], secondary_keyword)
-
-
 def generate_output():
     primary_keyword = primary_key.get(1.0, tk.END)
     secondary_keyword = secondary_key.get(1.0, tk.END)
     if primary_keyword is None:
         pass
     else:
-        search_wikipedia(primary_keyword[:-1], secondary_keyword[:-1])
+        results = search_wikipedia(primary_keyword[:-1], secondary_keyword[:-1])
+        print(results)
+        output_box.delete(1.0, tk.END)
+        output_box.insert(1.0, results)
 
 def search_wikipedia(primary_keyword, secondary_keyword=None):
     wiki_search = wiki.Wikipedia('en')
     results = wiki_search.page(primary_keyword).text.splitlines()
-    print(results)
     if secondary_keyword:
         for result in results:
             index = result.find(secondary_keyword)
             if index != -1:
-                results = result
-                break
+                return result
+
             else:
-                results = "Secondary Keyword Not Found" + results[0]
+                return "Secondary Keyword Not Found"
+
     else:
-        results = results[0]
-    output_box.delete(1.0, tk.END)
-    output_box.insert(1.0, results)
+        return results[0]
+
 
 def generate_csv():
     pass
